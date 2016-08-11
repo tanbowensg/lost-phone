619835843d25808b193a3b8b174cca8a3d596404
tanbowen <tanbowensg@gmail.com>
2016-08-11 20:45:11 +0800

    修改 app 的 dom 结构

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -52,39 +52,39 @@ class App extends Component {
 
   render() {
     return !this.props.error ? (
-<body>
-  <div className="topbar">
-    <div className="topbar-left">
-      <div className="topbar-item signal"><div className="dot full"></div><div className="dot full"></div><div className="dot full"></div><div className="dot empty"></div><div className="dot empty"></div>
-      </div>
-      <div className="topbar-item service-provider">中国联通</div>
-      <div className="topbar-item wifi">
-      wifi
-      </div>
-    </div>
-    <div className="topbar-right">
-      <div className="topbar-item bluetooth">
+      <div className="lost-phone">
+        <div className="topbar">
+          <div className="topbar-left">
+            <div className="topbar-item signal"><div className="dot full"></div><div className="dot full"></div><div className="dot full"></div><div className="dot empty"></div><div className="dot empty"></div>
+            </div>
+            <div className="topbar-item service-provider">中国联通</div>
+            <div className="topbar-item wifi">
+            wifi
+            </div>
+          </div>
+          <div className="topbar-right">
+            <div className="topbar-item bluetooth">
 
-        bluetooth
-      </div>
-      <div className="topbar-item battery">
-        <span>64%</span>
-        <div className="full">
-          <div className="left-power"></div>
+              bluetooth
+            </div>
+            <div className="topbar-item battery">
+              <span>64%</span>
+              <div className="full">
+                <div className="left-power"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="time">
+          4:53
+        </div>
+        <div className="date">
+          8月7日 星期四
+        </div>
+        <div className="slide-unlock">
+          > 滑动来解锁
         </div>
       </div>
-    </div>
-  </div>
-  <div className="time">
-    4:53
-  </div>
-  <div className="date">
-    8月7日 星期四
-  </div>
-  <div className="slide-unlock">
-    > 滑动来解锁
-  </div>
-</body>
     ) : this.props.children;
   }
 
