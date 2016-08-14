bafb94b37169239edb244d91b132f3b50dc0eedb
bowen tan <bowen tan>
2016-08-14 20:28:30 +0800

    基本完成了滑动毛玻璃效果，但还有bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,18 @@
 import React, { Component, PropTypes } from 'react';
-import ReactSwipe from 'react-swipe';
+import Swipe from '../mySwipe';
 import LockScreen from '../lockScreen';
 import bg from '../../public/bg.jpg';
-import s from './App.scss'
+import s from './App.scss';
 import Topbar from '../topbar';
 import Dial from '../dial';
 
-class App extends Component {
+const SwipeCfg = {
+  MinBright: 0.5,
+  MaxBlur: 20,
+  BlurStart: 0.5,
+};
 
+class App extends Component {
   static propTypes = {
     context: PropTypes.shape({
       insertCss: PropTypes.func,
@@ -18,10 +23,20 @@ class App extends Component {
     insertCss: PropTypes.func.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgStyle: {
+        filter: 'blur(0px) brightness(1)',
+      },
+      swipeIndex: 1,
+    };
+  }
+
   getChildContext() {
     const context = this.props.context;
     return {
-      insertCss: context.insertCss || emptyFunction,
+      insertCss: context.insertCss,
     };
   }
 
@@ -33,23 +48,103 @@ class App extends Component {
   componentWillUnmount() {
     this.removeCss();
   }
-  
+
+  // 改变背景的明度和模糊
+  onSwiping(progress) {
+    if (this.state.shouldChangeBg) {
+      if ((this.state.swipeIndex === 0 && progress < 0) ||
+        this.state.swipeIndex === 1 && progress > 0) {
+        return false;
+      }
+      const percent = Math.abs(progress);
+      let blur = 0;
+      let bright = 0;
+      switch (this.state.swipeIndex) {
+        // 密码盘界面的滑动
+        case 0:
+          if (percent < SwipeCfg.BlurStart) {
+            blur = SwipeCfg.MaxBlur *
+              (1 - percent / SwipeCfg.BlurStart);
+          }
+          bright = SwipeCfg.MinBright + (1 - SwipeCfg.MinBright) * percent;
+          break;
+        // 锁屏界面的滑动
+        case 1:
+          if (percent > SwipeCfg.BlurStart) {
+            blur = SwipeCfg.MaxBlur *
+              (percent - SwipeCfg.BlurStart) / SwipeCfg.BlurStart;
+          }
+          bright = 1 - (1 - SwipeCfg.MinBright) * percent;
+          break;
+        default:
+      }
+
+      // 检查blur是否超出最大值
+      if (SwipeCfg.MaxBlur * percent > SwipeCfg.MaxBlur) {
+        blur = SwipeCfg.MaxBlur;
+      }
+
+      // 检查brightness是否超出最小值
+      if (bright > 1) {
+        bright = 1;
+      } else if (bright < SwipeCfg.MinBright) {
+        bright = SwipeCfg.MinBright;
+      }
+
+      this.setState({
+        bgStyle: {
+          '-webkit-filter': `blur(${blur}px) 
+            brightness(${bright})`,
+        },
+      });
+    }
+    return false;
+  }
+
+  swipeCallback(index) {
+    if (index === 0) {
+      this.setState({
+        bgStyle: {
+          '-webkit-filter': `blur(${SwipeCfg.MaxBlur}) 
+            brightness(${SwipeCfg.MinBright})`,
+        },
+        swipeIndex: index,
+      });
+    } else if (index === 1) {
+      this.setState({
+        bgStyle: {
+          '-webkit-filter': `blur(0) 
+            brightness(1)`,
+        },
+        swipeIndex: index,
+      });
+    }
+  }
+
   render() {
+    const that = this;
+    const swipeOptions = {
+      startSlide: 1,
+      shortSwipes: false,
+      continuous: false,
+      swiping: that.onSwiping.bind(that),
+      // callback: that.swipeCallback.bind(that),
+      transitionEnd: that.swipeCallback.bind(that),
+    };
     return (
       <div className="lost-phone">
-        <img className="backgroud" src={bg}></img>
+        <img className="backgroud" src={bg} style={this.state.bgStyle}></img>
         <Topbar></Topbar>
         <main className="content">
-          <ReactSwipe className="carousel"
-            swipeOptions={{continuous: false}}
-            startSlide={1}>
+          <Swipe className="carousel"
+            swipeOptions={swipeOptions}>
             <div className="slider">
               <Dial></Dial>
             </div>
             <div className="slider">
               <LockScreen></LockScreen>
             </div>
-          </ReactSwipe>
+          </Swipe>
         </main>
       </div>
 
